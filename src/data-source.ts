eb27760824aceceac1f1e00209a96c84584c4d16
IbrahimGhorab <ibrahimabdallah81@gmail.com>
2023-08-06 22:37:42 +0300

    :)

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,7 +12,7 @@ config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.PGHOST,
-  port: +process.env.PGPORT!, // other way to defined it as number :Number(`${process.env.PGPORT}`),
+  port: +process.env.PGPORT!,  // other way to defined it as number :Number(`${process.env.PGPORT}`),
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
